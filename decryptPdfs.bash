594fedf66c3a3e6666b462f1fc95df18b79e66a0
Tom <tomask@x250.home>
2017-07-09 02:58:32 -0700

    Changing license to GPL 3.0

--- decryptPdfs.bash
+++ decryptPdfs.bash
@@ -8,8 +8,8 @@
 ##########################################################################
 # Date:    08 September 2016
 # Author:  Tomas Kuchta
-# License: Creative Commons CC0
-#          https://creativecommons.org/publicdomain/zero/1.0/legalcode
+# License: GNU General Public License v3.0
+#          http://www.gnu.org/licenses/gpl-3.0.txt
 ##########################################################################
 # Usage:
 # decryptPdfs.bash [directoryName] [directoryName] ..
